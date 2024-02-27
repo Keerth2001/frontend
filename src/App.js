e60cafec9eb11e91d819991a3e49b3759bcec202
[Keerthan N] <[keerthann18@gmail.com]>
2024-02-27 22:18:07 +0530

    updated new codes(27-02-2024)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Components/LoginSignup/login";
+import Products from "./Components/Products/products";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/home" element={<Products />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ src/Components/Products/products.js
@@ -0,0 +1,11 @@
+import MyNavbar from "../MyComponents/MyNavbar";
+
+function Products() {
+  return (
+    <div>
+      <MyNavbar />
+    </div>
+  );
+}
+
+export default Products;
